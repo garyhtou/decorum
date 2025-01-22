7d2287c19c55a856ad48ddad3a86cbdc30ba0229
Gary Tou <gary@garytou.com>
2025-01-22 00:19:20 -0800

    Fix `LeftNoLamps` condition
    Broken after allowing object slots to be empty

--- decorum/conditions/left_no_lamps.rb
+++ decorum/conditions/left_no_lamps.rb
@@ -2,7 +2,7 @@ module Decorum
   module Conditions
     class LeftNoLamps < ::Decorum::Condition
       def fulfilled?(player:, house:)
-        house.top_left_room.lamp.nil? && house.bottom_left_room.lamp.nil?
+        house.top_left_room.lamp.empty? && house.bottom_left_room.lamp.empty?
       end
     end
   end
